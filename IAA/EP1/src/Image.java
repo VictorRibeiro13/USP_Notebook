248e15339ba030aba412f52d55f632758e32990c
VictorRibeiro13 <victorribeiro.docs.13@gmail.com>
2020-11-29 19:35:57 -0300

    style: remove blanck spaces

--- IAA/EP1/src/Image.java
+++ IAA/EP1/src/Image.java
@@ -15,12 +15,10 @@ public class Image {
 	private Color bgColor;
 
 	public Image(int w, int h, int r, int g, int b){
-
 		setup(w, h, r, g, b);
 	}
 
 	public Image(int w, int h){
-
 		setup(w, h, 0, 0, 0);
 	}
 
@@ -41,68 +39,54 @@ public class Image {
 	}
 
 	public int getWidth(){
-
 		return width;
 	}
 	
 	public int getHeight(){
-
 		return height;
 	}
 
 	public void setBgColor(Color c){
-
 		bgColor = c;
 	}
 
-	public void setBgColor(int r, int g, int b){
-
+	public void setBgColor(int r, int g, int b) {
 		setBgColor(new Color(limit(r), limit(g), limit(b)));
 	}
 
-	public void setColor(Color c){
-
+	public void setColor(Color c) {
 		color = c;
 	}
 
-	public void setColor(int r, int g, int b){
-
+	public void setColor(int r, int g, int b) {
 		setColor(new Color(limit(r), limit(g), limit(b)));
 	}
 
-	public void clear(){
-
+	public void clear() {
 		graphics.setColor(bgColor);
 		graphics.fillRect(0, 0, width, height);
 	}
 
 	public void setPixel(int x, int y) {
-			
 		if(x >= 0 && y >= 0 && x < width && y < height){
-			
 			image.setRGB(x, y, color.getRGB());
 		}
 	}
 
-	public int getPixel(int x, int y){
-
+	public int getPixel(int x, int y) {
 		return image.getRGB(x, y); 
 	}
 
 	public void drawLine(int x1, int y1, int x2, int y2){
-
 		graphics.setColor(color);
 		graphics.drawLine(x1, y1, x2, y2);
 	}
 
 	public void save(String fileName){
-
-		try{
-
+		try {
 			ImageIO.write(image, "png", new File(fileName));
 		}
 		catch(IOException e){
-	
 			System.out.println("Unable to save image...");
 			e.printStackTrace();
 		}
