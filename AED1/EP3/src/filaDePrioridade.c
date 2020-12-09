ad1ef4932289fb97c1729967c40ceba98a999344
VictorRibeiro13 <victorribeiro.docs.13@gmail.com>
2020-12-09 19:52:04 -0300

    feat: add changeChildrenElementPosition function

--- AED1/EP3/src/filaDePrioridade.c
+++ AED1/EP3/src/filaDePrioridade.c
@@ -112,26 +112,40 @@ bool aumentarPrioridade(PFILA f, int id, float novaPrioridade) {
   return true;
 }
 
-bool reduzirPrioridade(PFILA f, int id, float novaPrioridade){
-  // if (id < 0 || id > f->maxElementos || f->arranjo[id] == NULL) return false;
-  //   PONT element = f->arranjo[id];
-  //   if (element->prioridade <= novaPrioridade) return false;
 
-  //   element->prioridade = novaPrioridade;
+void changeChildrenElementPosition(PFILA f, int index) {
+  int rightChildren = (int) floor((2 * index) + 1);
+  int leftChildren = (int) floor((2 * index) + 2);
+  PONT actualElement = f->heap[index];
 
-  //   int index = element->posicao;
-  //   int fatherIndex = (index-1) < 0 ? 0 : (int) floor((index-1)/2);
+  if (f->heap[rightChildren] != NULL && f->heap[rightChildren]->prioridade > actualElement->prioridade) { // pode dar errado, nesse caso colocar 2 ifs
+    f->heap[index] = f->heap[rightChildren];
+    f->heap[rightChildren] = actualElement;
 
-  //   while (f->heap[fatherIndex]->prioridade < element->prioridade) {
-  //     f->heap[index] = f->heap[fatherIndex];
-  //     f->heap[fatherIndex] = element;
+    f->heap[index]->posicao = index;
+    f->heap[rightChildren]->posicao = rightChildren;
 
-  //     f->heap[index]->posicao = index;
-  //     f->heap[fatherIndex]->posicao = fatherIndex; 
+    changeChildrenElementPosition(f, rightChildren);
+  }
 
-  //     index = fatherIndex;
-  //     fatherIndex = (int) floor((index-1)/2);
-  //   }
+  if (f->heap[leftChildren] != NULL && f->heap[leftChildren]->prioridade > actualElement->prioridade && index != 0) {
+    f->heap[index] = f->heap[leftChildren];
+    f->heap[leftChildren] = actualElement;
+
+    f->heap[index]->posicao = index;
+    f->heap[leftChildren]->posicao = leftChildren;
+
+    changeChildrenElementPosition(f, leftChildren);
+  }
+} 
+
+bool reduzirPrioridade(PFILA f, int id, float novaPrioridade) {
+  if (id < 0 || id > f->maxElementos || f->arranjo[id] == NULL) return false;
+  if (f->arranjo[id]->prioridade < novaPrioridade) return false;
+
+  f->arranjo[id]->prioridade = novaPrioridade;
+
+  changeChildrenElementPosition(f, f->arranjo[id]->posicao);
 
   return true;
 }
@@ -141,14 +155,13 @@ PONT removerElemento(PFILA f) {
   if (heapLength == 0) return NULL;
   
   PONT elementRemoved = f->heap[0];
-  f->heap[0] = NULL;
+  f->heap[0] = f->heap[f->elementosNoHeap-1];
+  f->heap[f->elementosNoHeap-1] = NULL;
 
-  for (int i = 0; i < heapLength; i++) {
-    f->heap[i] = f->heap[i+1];
-  }
+  changeChildrenElementPosition(f, 0);
 
   f->elementosNoHeap--;
-  
+  f->arranjo[elementRemoved->id] = NULL;
   return elementRemoved;
 }
 
@@ -157,4 +170,3 @@ bool consultarPrioridade(PFILA f, int id, float* resposta) {
   *resposta = f->arranjo[id]->prioridade;
   return true;
 }
-
