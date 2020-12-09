b0bca041f3f24d7c11d78a9c48b5051fb652fe70
VictorRibeiro13 <victorribeiro.docs.13@gmail.com>
2020-12-09 01:24:04 -0300

    feat: add function removerElementos

--- AED1/EP3/src/filaDePrioridade.c
+++ AED1/EP3/src/filaDePrioridade.c
@@ -114,59 +114,43 @@ bool aumentarPrioridade(PFILA f, int id, float novaPrioridade) {
 }
 
 bool reduzirPrioridade(PFILA f, int id, float novaPrioridade){
-  // if (id < 0 || id > f->maxElementos || f->arranjo[id] == NULL) return false;
-  //   PONT element = f->arranjo[id];
-  //   if (element->prioridade <= novaPrioridade) return false;
+  if (id < 0 || id > f->maxElementos || f->arranjo[id] == NULL) return false;
+    PONT element = f->arranjo[id];
+    if (element->prioridade <= novaPrioridade) return false;
 
-  //   element->prioridade = novaPrioridade;
+    element->prioridade = novaPrioridade;
 
-  //   int index = element->posicao;
-  //   int fatherIndex = (index-1) < 0 ? 0 : (int) floor((index-1)/2);
+    int index = element->posicao;
+    int fatherIndex = (index-1) < 0 ? 0 : (int) floor((index-1)/2);
 
-  //   while (f->heap[fatherIndex]->prioridade < element->prioridade) {
-  //     f->heap[index] = f->heap[fatherIndex];
-  //     f->heap[fatherIndex] = element;
+    while (f->heap[fatherIndex]->prioridade < element->prioridade) {
+      f->heap[index] = f->heap[fatherIndex];
+      f->heap[fatherIndex] = element;
 
-  //     f->heap[index]->posicao = index;
-  //     f->heap[fatherIndex]->posicao = fatherIndex; 
+      f->heap[index]->posicao = index;
+      f->heap[fatherIndex]->posicao = fatherIndex; 
 
-  //     index = fatherIndex;
-  //     fatherIndex = (int) floor((index-1)/2);
-  //   }
+      index = fatherIndex;
+      fatherIndex = (int) floor((index-1)/2);
+    }
 
   return true;
 }
 
 PONT removerElemento(PFILA f) {
-  if (tamanho(f) == 0) return NULL;
-
-  int index = f->heap[0];
-
-  PONT actualElement = f->heap[1];
-
-  for (int i = 1; i < tamanho(f); i++) {
-    /* code */
-  }
+  int heapLength = tamanho(f);
+  if (heapLength == 0) return NULL;
   
+  PONT elementRemoved = f->heap[0];
+  f->heap[0] = NULL;
 
-  while (actualElement != NULL && actualElement->posicao != f->maxElementos-1) {
-
-    f->heap[index] = f->heap[fatherIndex];
-    f->heap[fatherIndex] = element;
-
-    f->heap[index]->posicao = index;
-    f->heap[fatherIndex]->posicao = fatherIndex; 
-
-    index = fatherIndex;
-    fatherIndex = (int) floor((index-1)/2);
+  for (int i = 0; i < heapLength; i++) {
+    f->heap[i] = f->heap[i+1];
   }
 
-  return true;
-
-
-
+  f->elementosNoHeap--;
   
-  return NULL;
+  return elementRemoved;
 }
 
 bool consultarPrioridade(PFILA f, int id, float* resposta){
